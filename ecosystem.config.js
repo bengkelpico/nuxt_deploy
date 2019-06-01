a57015f00551a678cc5a5fa19edf5a2c1b74779e
Julian Prasetyo <4065455+picobug@users.noreply.github.com>
2019-06-01 13:27:46 +0700

    Change to Max Value

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
       cwd: './current',
       script: './node_modules/.bin/nuxt',
       args: 'start',
-      instances: 'start',
+      instances: 'max',
       autorestart: true,
       watch: false,
       max_memory_restart: '256M',
